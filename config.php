9a3d8724b24f65bd514e8d0c7f566b35f39883a5
reginaldo <reginaldobrain@gmail.com>
2024-08-14 15:10:37 -0300

    segundo

--- config.php
+++ config.php
@@ -5,3 +5,5 @@ define('DB_NAME',       'loja_api');
 define('DB_CHARSET',    'utf8');
 define('DB_USERNAME',   'user_loja_api');
 define('DB_PASSWORD',   '5ere58bA5e78yIKiguM7y5L36EN8YE');
+
+linha nova
